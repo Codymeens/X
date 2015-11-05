c969fdf23fe26b8a020d50aba0c517b3cc4652eb
it's a me Cody <cody_meens@msn.com>
2015-11-05 16:56:39 +0100

    UMLviewer

---
+++ UMLviewapp1/UMLviewapp1/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace UMLviewapp1
+{
+	[Activity (Label = "UMLviewapp1", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			ImageView afbeelding = FindViewById<ImageView> (Resource.Id.afbeelding);
+			Button start= FindViewById<Button> (Resource.Id.start);
+			
+			start.Click += delegate {
+				StartActivity(typeof(activity_start_knop));
+			};
+		}
+	}
+}
+
+
---
+++ UMLviewapp1/UMLviewapp1/activity_start_knop.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace UMLviewapp1
+{
+	[Activity (Label = "activity_start_knop")]			
+	public class activity_start_knop : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.layout_start);
+			// Create your application here
+
+			ImageView afbeelding_UML = FindViewById<ImageView> (Resource.Id.afbeelding_UML);
+			EditText aantekeningen = FindViewById<EditText> (Resource.Id.aantekeningen);
+			RatingBar ratingbar = FindViewById<RatingBar> (Resource.Id.ratingBar);
+			Button open = FindViewById<Button> (Resource.Id.open);
+			Button download = FindViewById<Button> (Resource.Id.download);
+			Button upload = FindViewById<Button> (Resource.Id.upload);
+
+				
+		}
+	}
+}
+
